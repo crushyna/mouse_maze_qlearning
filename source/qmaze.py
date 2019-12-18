0738d33b9e7ac9559a52361c38c1348343050e50
crushyna <crushyna@gmail.com>
2019-12-18 19:51:15 +0100

    neural network and experience class added requires some fixes

--- source/qmaze.py
+++ source/qmaze.py
@@ -1,7 +1,29 @@
-from typing import Type
-
-from main import ProjectConstants as const
+from dataclasses import dataclass
+from typing import Type, List, Tuple
 import numpy as np
+
+
+@dataclass
+class Constants:
+    visited_mark = 0.8  # Cells visited by the mouse will be painted by gray 0.8
+    mouse_mark = 0.5  # The current mouse cell will be painted by gray 0.5
+    LEFT = 0
+    UP = 1
+    RIGHT = 2
+    DOWN = 3
+
+    # Actions dictionary
+    actions_dict = {
+        LEFT: 'left',
+        UP: 'up',
+        RIGHT: 'right',
+        DOWN: 'down',
+    }
+
+    num_actions = len(actions_dict)
+
+    # Exploration factor
+    epsilon = 0.1
 
 
 class Qmaze(object):
@@ -31,7 +53,7 @@ class Qmaze(object):
         self.maze = np.copy(self._maze)
         nrows, ncols = self.maze.shape
         row, col = mouse
-        self.maze[row, col] = const.mouse_mark
+        self.maze[row, col] = Constants.mouse_mark
         self.state = (row, col, 'start')
         self.min_reward = -0.5 * self.maze.size
         self.total_reward = 0
@@ -50,13 +72,13 @@ class Qmaze(object):
             nmode = 'blocked'
         elif action in valid_actions:
             nmode = 'valid'
-            if action == const.LEFT:
+            if action == Constants.LEFT:
                 ncol -= 1
-            elif action == const.UP:
+            elif action == Constants.UP:
                 nrow -= 1
-            if action == const.RIGHT:
+            if action == Constants.RIGHT:
                 ncol += 1
-            elif action == const.DOWN:
+            elif action == Constants.DOWN:
                 nrow += 1
         else:  # invalid action, no change in mouse position
             mode = 'invalid'
@@ -101,7 +123,7 @@ class Qmaze(object):
                     canvas[r, c] = 1.0
         # draw the mouse
         row, col, valid = self.state
-        canvas[row, col] =const.mouse_mark
+        canvas[row, col] = Constants.mouse_mark
         return canvas
 
     def game_status(self):
